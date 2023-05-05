309901a18b87331f3a59cea149c5e185d9ee86d1
Repro12 <111174041+Repro12@users.noreply.github.com>
2023-05-05 16:45:47 +0530

    Add files via upload

---
+++ Garage.js
@@ -0,0 +1,25 @@
+function preload()
+{
+    img = loadImage('Garage.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(380, 380);
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+
+function draw()
+{
+    image(video, 0, 0, 380, 380);
+}
